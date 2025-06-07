8ba9cc2228c88ceb79376fa51cfe7d3bc57fe9dc
Subhrajyoty Roy <subhrajyotyroy@gmail.com>
2025-06-07 14:25:43 +0000

    update example

--- examples/a_plus_b_square.py
+++ examples/a_plus_b_square.py
@@ -21,12 +21,14 @@ scene = Scene(width=1920, height=1088)  # blank scene (viewport = 1777, 1000)
 FONT_NAME = "feasibly"
 VALUE_A = 100
 VALUE_B = 200
+A_PLUS_B = VALUE_A + VALUE_B
+sq_top_left = (300, 500)
 
 
 # scene 1: draw the title and fade in
 title_text = Math(
     tex_expression=r"$(a + b)^2 = ?$",
-    position=(300, 500),
+    position=sq_top_left,
     font_size=192,
     stroke_style=StrokeStyle(color=BLUE, width=2),
     glow_dot_hint={"color": BLUE, "radius": 5},
@@ -46,8 +48,8 @@ scene.add(
 
 # scene 2: draw the square
 a_plus_b_sq = Square(
-    top_left=(300, 500),
-    side_length=(VALUE_A + VALUE_B),
+    top_left=sq_top_left,
+    side_length=A_PLUS_B,
     stroke_style=StrokeStyle(color=BLACK, width=2),
     fill_style=FillStyle(color=BLUE, hachure_gap=10),
     sketch_style=SketchStyle(roughness=3),
@@ -64,7 +66,10 @@ a_plus_b_label_group = DrawableGroup(
             stroke_style=StrokeStyle(color=BLACK, width=2),
             font_name=FONT_NAME,
         )
-        for pos in [(350, 400), (650, 650)]
+        for pos in [
+            (sq_top_left[0] + A_PLUS_B / 2, sq_top_left[1] - 100), 
+            (sq_top_left[0] + A_PLUS_B + 50, sq_top_left[1] + A_PLUS_B / 2)
+        ]
     ]
 )
 a_plus_b_sq_label = Math(
@@ -96,19 +101,13 @@ scene.add(
 
 # scene 3: draw the horizontal and vertical lines
 vert_line = Line(
-    start=(a_plus_b_sq.top_left[0] + VALUE_A, a_plus_b_sq.top_left[1]),
-    end=(
-        a_plus_b_sq.top_left[0] + VALUE_A,
-        a_plus_b_sq.top_left[1] + a_plus_b_sq.side_length,
-    ),
+    start=(sq_top_left[0] + VALUE_A, sq_top_left[1]),
+    end=(sq_top_left[0] + VALUE_A, sq_top_left[1] + A_PLUS_B),
     stroke_style=StrokeStyle(color=BLACK, width=2),
 )
 horiz_line = Line(
-    start=(a_plus_b_sq.top_left[0], a_plus_b_sq.top_left[1] + VALUE_A),
-    end=(
-        a_plus_b_sq.top_left[0] + a_plus_b_sq.side_length,
-        a_plus_b_sq.top_left[1] + VALUE_A,
-    ),
+    start=(sq_top_left[0], sq_top_left[1] + VALUE_A),
+    end=(sq_top_left[0] + A_PLUS_B, sq_top_left[1] + VALUE_A),
     stroke_style=StrokeStyle(color=BLACK, width=2),
 )
 horiz_vert_line_group = DrawableGroup(elements=[vert_line, horiz_line])
@@ -118,32 +117,111 @@ scene.add(
 
 # scene 4: draw the a_sq and b_sq
 a_sq = Square(
-    top_left=a_plus_b_sq.top_left,
+    top_left=sq_top_left,
     side_length=VALUE_A,
     stroke_style=StrokeStyle(color=BLACK, width=2),
-    fill_style=FillStyle(color=RED, hachure_gap=10),
+    fill_style=FillStyle(color=RED, hachure_gap=10, hachure_angle = 135),
     sketch_style=SketchStyle(roughness=3),
 )
 scene.add(SketchAnimation(start_time=8.5, duration=2), drawable=a_sq)
 
 # add labels, they zoom out and move to the point
+a_label_group = DrawableGroup(
+    elements=[
+        Math(
+            tex_expression=r"$a$",
+            position=pos,
+            font_size=60,
+            stroke_style=StrokeStyle(color=BLACK, width=2),
+            font_name=FONT_NAME,
+        )
+        for pos in [
+            (sq_top_left[0] + VALUE_A / 2, sq_top_left[1] - 100), 
+            (sq_top_left[0] - 100, sq_top_left[1] + VALUE_A / 2)
+        ]
+    ]
+)
+a_sq_label = Math(
+    tex_expression=r"$= a^2$",
+    position=(780, 200),
+    font_size=96,
+    stroke_style=StrokeStyle(color=BLACK, width=2),
+    font_name=FONT_NAME,
+)
+
+# the other labels should by this time move into this point and zoom out
+scene.add(
+    CompositeAnimationEvent(
+        events=[
+            SketchAnimation(start_time=10.5, duration=1),
+            ZoomOutAnimation(start_time=11.5, duration=2),
+            TranslateToAnimation(
+                start_time=13.5, duration=2, data={"point": a_sq_label.position}
+            ),
+        ]
+    ),
+    drawable=a_label_group,
+)
+scene.add(
+    event=FadeInAnimation(start_time=13.5, duration=1),
+    drawable=a_sq_label,
+)
 
 
 b_sq = Square(
-    top_left=(
-        a_plus_b_sq.top_left[0] + VALUE_A,
-        a_plus_b_sq.top_left[1] + VALUE_A,
-    ),
+    top_left=(sq_top_left[0] + VALUE_A, sq_top_left[1] + VALUE_A),
     side_length=VALUE_B,
     stroke_style=StrokeStyle(color=BLACK, width=2),
     fill_style=FillStyle(color=RED, hachure_gap=10),
     sketch_style=SketchStyle(roughness=3),
 )
-scene.add(SketchAnimation(start_time=10.5, duration=2), drawable=b_sq)
+scene.add(SketchAnimation(start_time=13.5, duration=2), drawable=b_sq)
+
+# add labels, they zoom out and move to the point
+b_label_group = DrawableGroup(
+    elements=[
+        Math(
+            tex_expression=r"$b$",
+            position=pos,
+            font_size=60,
+            stroke_style=StrokeStyle(color=BLACK, width=2),
+            font_name=FONT_NAME,
+        )
+        for pos in [
+            (sq_top_left[0] + VALUE_A + VALUE_B / 2, sq_top_left[1] + A_PLUS_B + 100), 
+            (sq_top_left[0] + A_PLUS_B + 100, sq_top_left[1] + VALUE_A + VALUE_B / 2)
+        ]
+    ]
+)
+b_sq_label = Math(
+    tex_expression=r"$+ b^2$",
+    position=(850, 200),
+    font_size=96,
+    stroke_style=StrokeStyle(color=BLACK, width=2),
+    font_name=FONT_NAME,
+)
+
+# the other labels should by this time move into this point and zoom out
+scene.add(
+    CompositeAnimationEvent(
+        events=[
+            SketchAnimation(start_time=15.5, duration=1),
+            ZoomOutAnimation(start_time=16.5, duration=2),
+            TranslateToAnimation(
+                start_time=18.5, duration=2, data={"point": b_sq_label.position}
+            ),
+        ]
+    ),
+    drawable=b_label_group,
+)
+scene.add(
+    event=FadeInAnimation(start_time=20.5, duration=1),
+    drawable=b_sq_label,
+)
 
 
 # save the scene
 output_path = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), "output", "a_plus_b_square.mp4"
 )
-scene.render(output_path, max_length=12)
+scene.render(output_path, max_length=21)
